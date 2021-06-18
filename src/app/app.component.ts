bc39621ec3c942e3a3489d3bf9f10054ec923d3f
krishna <krishna59.kp27@gmail.com>
2021-06-18 17:04:22 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'application';
+  title = 'Application';
+  num1: string = "";
+  num2: string = "";
+  result1: any;
+  result2: any;
+  result3: any;
+  result4: any;
+
+  add() {
+    this.result1 = parseInt(this.num1) + parseInt(this.num2);
+  }
+  sub() {
+    this.result2 = parseInt(this.num1) - parseInt(this.num2);
+  }
+  mul() {
+    this.result3 = parseInt(this.num1) * parseInt(this.num2);
+  }
+  div() {
+    this.result4 = parseInt(this.num1) / parseInt(this.num2);
+  }
 }
+
